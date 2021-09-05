a1a80dbb37864d51ae6ae10d5eb567570776db5d
RohitJain001 <59856865+RohitJain001@users.noreply.github.com>
2021-09-05 16:40:19 +0530

    music player

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+//import 'react-h5-audio-player/lib/styles.less' Use LESS
+//import 'react-h5-audio-player/src/styles.scss' Use SASS
+
 const Home=()=>{
     return(
-        <><h1 className="text-white">Hello home</h1>
-        <h1 className="text-white">Hello home</h1>
-        <h1 className="text-white">Hello home</h1>
-        <h1 className="text-white">Hello home</h1>
-        <h1 className="text-white">Hello home</h1>
-        <h1 className="text-white">Hello home</h1>
-        <h1 className="text-white">Hello home</h1>
-        <h1 className="text-white">Hello home</h1>
-        <h1 className="text-white">Hello home</h1>
-        <h1 className="text-white">Hello home</h1>
-        <h1 className="text-white">Hello home</h1>
-        <h1 className="text-white">Hello home</h1>
-        <h1 className="text-white">Hello home</h1>
-      
-        </>
+    <>
+        <div className="container-fluid">
+            <div className="Row">
+                <div className="col-5 mx-auto">
+                    <div className="row">
+                        <div class="col-md-6 mt-5 pt-5 order-2 order-lg-1">
+                            //first dabba 
+                            <AudioPlayer
+                            autoPlay
+                            src="C:/Users/DELL/Desktop/sep/src/songs/brown.mp4"
+                            onPlay={e => console.log("onPlay")}
+                            // other props here
+                        />
+                        </div>
+                    </div>
+                    <div className="col-md-6 pt-2 mt-3 order-lg-2 order-1">
+                        /* second dabba */
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
         
     )
 };
